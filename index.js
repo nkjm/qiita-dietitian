13514ad9c72ccbe00a38d6d60e49d308b9b75a19
nkjm <nkjm.kzk@gmail.com>
2016-11-28 14:58:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------------
+// モジュールのインポート
+var express = require('express');
+var app = express();
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+var port = (process.env.PORT || 3000);
+var server = app.listen(port, function() {
+    console.log('Node is running on port ' + port);
+});
+
+// -----------------------------------------------------------------------------
+// ルーター設定
+app.get('/', function(req, res, next){
+    res.send('Node is running on port ' + port);
+});
