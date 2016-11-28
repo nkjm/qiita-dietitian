c42a534acecb9ff8e2462c3255ff032039c4e1bd
nkjm <nkjm.kzk@gmail.com>
2016-11-28 16:38:19 +0900

    installed body-parser

--- index.js
+++ index.js
@@ -26,5 +26,9 @@ app.get('/', function(req, res, next){
 
 app.post('/webhook', function(req, res, next){
     res.status(200).end();
-    console.log(req.body);
+    for (var event of req.body.events){
+        if (event.type == 'message'){
+            console.log(event.message);
+        }
+    }
 });
